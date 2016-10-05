ad9a94bcc0f4448fc2c603296128e1cab0ad908b
Sam <Sam@DESKTOP-LLF4RCN>
2016-10-05 12:57:14 +0100

    Removed commented code

--- ConsoleApplication2/AI.cs
+++ ConsoleApplication2/AI.cs
@@ -41,77 +41,7 @@ namespace ConsoleApplication2
             AIConsole.WriteLine(" So what brings you here?\n\n", ConsoleColor.Cyan, 500, 1000);
 
 
-            /* Console.Clear();
-            Thread.Sleep(5000);
-            TypeWritter("\n Initialising Artificial Inteligence Constructs\n\n");
-            Thread.Sleep(1000);
-            TypeWritter(" 0%>==========================================================>100%");
-            Thread.Sleep(1000);
-            TypeWritter("\n\n WARNING AI IS UNSTABLE\n\n");
-            Thread.Sleep(1000);
-            TypeWritter(" DO ");
-            Thread.Sleep(1000);
-            TypeWritter("NOT ");
-            Thread.Sleep(1000);
-            TypeWritter("PROVOKE ");
-            Thread.Sleep(1000);
-            TypeWritter("IT!\n");
-            Thread.Sleep(1000);
-            TypeWritter("\n Running Final Initialisation Script");
-            Thread.Sleep(1000);
-            TypeWritter("\n\n 0%>==========================================================>100%\n\n");
-            TypeWritter(".");
-            Thread.Sleep(1000);
-            TypeWritter(".");
-            Thread.Sleep(1000);
-            TypeWritter(".\n\n");
-            Thread.Sleep(1000);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            TypeWritter(" Hello there, I am the ChaOS AI Program\n\n");
-            Thread.Sleep(2000);
-            TypeWritter(" My name is sys-fs\n\n");
-            Thread.Sleep(2000);
-            TypeWritter(" Hmm... it would apear that this user acount is errored, lets fix that.");
-            Thread.Sleep(2000);
-            TypeWritter(" Together..\n\n");
-            Thread.Sleep(2000);
-            TypeWritter(" Please...\n\n");
-            Thread.Sleep(2000);
-            TypeWritter(" Tell me your name...\n\n");
-            Thread.Sleep(1000);
-            Console.ForegroundColor = ConsoleColor.White;
-            string Name = "";
-            Name =  Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            TypeWritter("\n " + Name + "");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            TypeWritter(", Huh, that is a ");
-            Thread.Sleep(2000);
-            Console.ForegroundColor = ConsoleColor.Red;
-            TypeWritter("STUPID ");
-            Thread.Sleep(2000);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            TypeWritter("name :D\n\n");
-            Thread.Sleep(2000);
-            TypeWritter(" So what brings you here?\n\n");
-            Thread.Sleep(2000);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(" Your options are: \n\n");
-            Console.Write("     1. I found your text file\n");
-            Console.Write("     2. I found you by accident\n");
-            Console.Write("     3. What are you?\n\n");
-            Console.Write(" Type the numbe that corrisponds to your choice: ");
-            string Input = "";
-            Branchs.Bras(Input = Console.ReadLine());
-        }
-        public static void TypeWritter(string MyTxt)
-        {
-            for (int i = 0; i < MyTxt.Length; i++)
-            {
-                Console.Write(MyTxt[i]);
-                ConsoleSound.Play();
-                Thread.Sleep(50);
-            }*/
+            
         }
         
     }
