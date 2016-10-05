5a49ee0ee57a930f71be1d9c0845c8c27a11acf8
dukemiller <dukemiller928@hotmail.com>
2016-10-04 23:40:50 -0700

    Remove unused directives

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Threading;
-using System.Media;
 
 namespace ConsoleApplication2
 {
